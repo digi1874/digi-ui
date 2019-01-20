52787e02514a37370e335d8b944161623a9ce130
林振辉 <315713565@163.com>
2019-01-20 16:00:23 +0800

    Params

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,23 +1,18 @@
-import { objectTag, arrayTag } from '../toStringTag'
+import { arrayTag } from '../toStringTag'
+import { Params } from '../Params'
 
 import './index.scss'
 
 const className = 'card'
 
-export class Card {
+export class Card extends Params {
   /**
    * 卡片
    * @param   {Any} params - 元素属性
    * @returns { Object }   - { (...params | child: params), className: [ 'card', (params.className | ...params.className) ] }
    */
   constructor (params) {
-    if (toString.call(params) === objectTag) {
-      Object.keys(params).forEach(key => {
-        this[key] = params[key]
-      })
-    } else {
-      this.child = params
-    }
+    super(params)
 
     if (toString.call(this.className) !== arrayTag) {
       this.className = [ className ].concat([ this.className ])
